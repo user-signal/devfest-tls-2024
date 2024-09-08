639cc9d304724e5e079b23e438ecde5339b86324
Frédéric Cabestre <frederic.cabestre@sigusr.net>
2024-09-08 11:24:41 +0200

    Refactor example Kotlin code

--- code/Kotlin/src/Main.kt
+++ code/Kotlin/src/Main.kt
@@ -1,22 +1,41 @@
-import com.sun.tools.javac.jvm.ByteCodes.ret
-
 fun main() {
-    val a = foo(3)
-    val b = a(4)
-    println(b)
 
-    val c = bar(3)
-    val d = c(4)
-    println(d)
+    println(filter(listOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)) { it % 2 == 0 })
+    println(filter(listOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10), { it % 2 == 0 }))
+    println(filter(listOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10), { int -> int % 2 == 0 }))
+
+    val sieve: (Int) -> Boolean = parametricSieve(3)
+    println(filter(listOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10), sieve))
 }
 
-fun foo(x: Int) : (Int) -> Boolean = { y: Int -> x < y }
-
-fun bar(x: Int) : (Int) -> Boolean {
+fun parametricSieve(x: Int): (Int) -> Boolean {
     val c = longComputation(x)
-    return { y: Int -> x.mod(y) == 0}
+    return { y: Int -> c % y == 0 }
 }
+
+//val sieve: (Int) -> Boolean = parametricSieve(longComputation(3))
+//fun parametricSieve(p: Int): (Int) -> Boolean {
+//    return { y: Int -> p % y == 0 }
+//}
+
+fun parametricSieve(x: Int, p: Int): (Int) -> Boolean = { p % it == 0 }
 
 fun longComputation(x: Int): Int {
     return x * x
+}
+
+fun filter(list: List<Int>, predicate: (Int) -> Boolean): List<Int> {
+    fun filterAux(list: List<Int>, predicate: (Int) -> Boolean, acc: List<Int>): List<Int> =
+        when (list.isEmpty()) {
+            true -> acc
+            false -> {
+                val first = list.first()
+                if (predicate(first)) {
+                    filterAux(list.drop(1), predicate, acc + first)
+                } else {
+                    filterAux(list.drop(1), predicate, acc)
+                }
+            }
+        }
+    return filterAux(list, predicate, emptyList())
 }
