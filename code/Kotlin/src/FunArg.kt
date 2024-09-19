e6f6e1e291b8364ca2128c90b0547d3c1b190203
Frédéric Cabestre <frederic.cabestre@sigusr.net>
2024-09-19 09:29:59 +0200

    Furnarg problem code

---
+++ code/Kotlin/src/FunArg.kt
@@ -0,0 +1,11 @@
+fun main() {
+    println(consall("beat", listOf("harvard", "yale")))
+}
+
+fun <T, U> map(fn: (T) -> U, x: List<T>): List<U> =
+    when {
+        x.isEmpty() -> emptyList()
+        else -> listOf(fn(x.first())) + map(fn, x.drop(1))
+    }
+
+fun <T> consall(x: T, ys: List<T>) = map({ y -> listOf(x, y) }, ys)
