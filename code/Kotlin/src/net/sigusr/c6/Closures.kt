cd3f2fa3365bb24390267a479073f84e9f78fb5b
Frédéric Cabestre <frederic.cabestre@sigusr.net>
2024-10-06 20:29:03 +0200

    JSR335 and invokeDynamic

--- code/Kotlin/src/net/sigusr/c6/Closures.kt
+++ code/Kotlin/src/net/sigusr/c6/Closures.kt
@@ -6,7 +6,7 @@ fun longComputation(x: Int): Int {
 
 fun parametricPredicateLambda(c: Int, yy: Int): Boolean = yy % c == 0
 
-data class Closure<C, I, O>(val capture:C, val lambda: (C, I) -> O)
+data class Closure<C, I, O>(val capture:C, val code: (C, I) -> O)
 
 fun parametricPredicate(x: Int): Closure<Int, Int, Boolean> {
     val c = longComputation(x)
@@ -16,5 +16,5 @@ fun parametricPredicate(x: Int): Closure<Int, Int, Boolean> {
 fun main() {
     val c = 42
     val predicate: Closure<Int, Int, Boolean> = parametricPredicate(3)
-    if (predicate.lambda(predicate.capture, 81)) println("Ok") else println("Ko")
+    if (predicate.code(predicate.capture, 81)) println("Ok") else println("Ko")
 }
