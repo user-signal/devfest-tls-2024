544558b3fc7c5e857dbac919b3c4a530e9f9a6c6
Frédéric Cabestre <frederic.cabestre@sigusr.net>
2024-11-02 12:05:14 +0100

    InvokeDynamic schema

---
+++ code/Java/src/net/sigusr/c3/Main3.java
@@ -0,0 +1,42 @@
+import java.util.function.IntPredicate;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public static class Closure implements IntPredicate {
+
+    private final int c;
+
+    public Closure(int c) {
+        this.c = c;
+    }
+
+    public static boolean parametricPredicateLambda(int c, int yy) {
+        return yy % c == 0;
+    }
+
+    @Override
+    public boolean test(int value) {
+        return parametricPredicateLambda(c, value);
+    }
+}
+
+public static int longComputation(int x) {
+    return x * x;
+}
+
+public static IntPredicate parametricPredicate(int x) {
+    var c = longComputation(x);
+    return new Closure(c);
+}
+
+void main() {
+    var aStream = IntStream.rangeClosed(0, 100);
+    var predicate = parametricPredicate(3);
+    IntStream filteredStream = aStream.filter(predicate);
+    System.out.println(
+            filteredStream.
+                    mapToObj(Integer::toString).
+                    collect(Collectors.joining(",", "[", "]")));
+}
+
+// [0,9,18,27,36,45,54,63,72,81,90,99]
